0dff2929530143d91a69a215fbe88d8cbb893499
Reed Espinosa <reedespinosa@umbc.edu>
2014-11-15 16:03:24 -0500

    finished first working tweetManip

--- tweetManip.py
+++ tweetManip.py
@@ -2,17 +2,39 @@ from hyphen import Hyphenator, dict_info
 from hyphen.dictools import *
 h_en = Hyphenator('en_US')
 
+class buildHaiku():
+    def __init__(self):
+        self.finalHaiku = list()
+        self.nLines = 0
+    def newTweet(self, tweetText):
+        tweetObj = checkTweet(tweetText)
+        tweetObj.replaceHashtag()
+        tweetObj.findWords()
+        Nsyls = 5 + 2*(self.nLines%2)
+        line = tweetObj.checkSylbls(Nsyls)
+        print(line)
+        if line:
+            self.finalHaiku.append(line)
+            self.nLines += 1
+        if self.nLines == 3:
+            return self.finalHaiku
+        else:
+            return list()
+               
+
 class checkTweet():
-    def __init__(self, text = 'Defualt Tweet', repeats = False):
+    def __init__(self, text = 'Defualt Tweet'):
         self.text = text
-        self.repeats = repeats
         self.textWords = list()
         self.nWords = 0
+
     def replaceHashtag(self):
         self.text = self.text.replace('#', 'hashtag ')
+
     def findWords(self):
         self.textWords=self.text.split()
         self.nWords = len(self.textWords)
+
     def checkSylbls(self, Nsyls):
         i = 0
         sylsCount = 0;
@@ -25,10 +47,3 @@ class checkTweet():
             return ' '.join(self.textWords[:i])
         else:
             return ''
-            
-        	
-		        
-
-	        
-        
-
