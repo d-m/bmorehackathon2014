a4f6ca38aa4e4d3cb0d30478ae9cbb01edf2f89c
Reed Espinosa <reedespinosa@umbc.edu>
2014-11-15 16:52:22 -0500

    added more generic quality control

--- tweetManip.py
+++ tweetManip.py
@@ -8,19 +8,20 @@ class buildHaiku():
         self.nLines = 0
     def newTweet(self, tweetText):
         tweetObj = checkTweet(tweetText)
-        tweetObj.replaceHashtag()
-        tweetObj.findWords()
-        Nsyls = 5 + 2*(self.nLines%2)
-        line = tweetObj.checkSylbls(Nsyls)
-        print(line)
-        if line:
-            self.finalHaiku.append(line)
-            self.nLines += 1
-        if self.nLines == 3:
-            return self.finalHaiku
-        else:
-            return list()
-               
+        keepGoing = tweetObj.qualityControl()
+        if keepGoing:
+            tweetObj.findWords()
+            Nsyls = 5 + 2*(self.nLines%2)
+            line = tweetObj.checkSylbls(Nsyls)
+            if line:
+                self.finalHaiku.append(line)
+                self.nLines += 1
+            if self.nLines == 3:
+                return self.finalHaiku
+            else:
+                return list()
+        else
+            return list()               
 
 class checkTweet():
     def __init__(self, text = 'Defualt Tweet'):
@@ -28,8 +29,19 @@ class checkTweet():
         self.textWords = list()
         self.nWords = 0
 
+    def qualityControl(self):
+        self.replaceHashtag(self)
+        if not self.retweetCheck(self)
+            return false
+#         do some more checks
+        return true
+    
     def replaceHashtag(self):
         self.text = self.text.replace('#', 'hashtag ')
+
+    def retweetCheck(self)
+#         check if retweet 
+#           if so return false else return true
 
     def findWords(self):
         self.textWords=self.text.split()
@@ -40,6 +52,7 @@ class checkTweet():
         sylsCount = 0;
         while i < self.nWords and sylsCount < Nsyls:
             wordSyls = len(h_en.syllables(self.textWords[i]))
+            print(h_en.syllables(self.textWords[i]))
             sylsCount = sylsCount + max(wordSyls, 1)
             i += 1
     
