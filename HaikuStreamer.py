ed22bdf132c2ef43cc17b85b0b3542219fba1130
Reed Espinosa <reedespinosa@umbc.edu>
2014-11-15 21:45:14 -0500

    connecting tweetManip and HaikuStreamer

--- HaikuStreamer.py
+++ HaikuStreamer.py
@@ -1,13 +1,20 @@
 from twython import TwythonStreamer
 from nltk.corpus import wordnet as wn
-
+from tweetManip import buildHaiku
 
 class HaikuStreamer(TwythonStreamer):
+    def __init__(self, *args, **kwargs):
+        super( HaikuStreamer, self ).__init__(*args, **kwargs)
+        self.newHaiku = buildHaiku()
 
     def on_success(self, data):
         if data.has_key('text'):
-            print data['text']
-
+            result = self.newHaiku.newTweet(data['text'])
+            if result:
+                print(result)
+                self.disconnect()
+                
+            
     def on_error(self, status_code, data):
         print status_code, data
         self.disconnect()
